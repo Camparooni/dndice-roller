ad0827c3060bb399048ec9fdc5c7b170f2043906
Taylor Campanelli <tayascamp@yahoo.com>
2023-07-27 17:34:23 -0700

    updated checkboxes to radios and added javascript for images to appear in the center of box2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,39 @@
 const rollbtn = document.getElementById('rollbtn');
 const currentRollInput = document.getElementById('currentRoll');
 const previousRollsDiv = document.getElementById('previousRolls');
-const diceInputs = document.querySelectorAll('input[type="checkbox"]');
+const diceInputs = document.querySelectorAll('input[type="radio"]'); // Corrected the selector for radio inputs
+const diceImage = document.getElementById('diceimage');
 let selectedDice = 6; // Default to D6
 
 let previousRolls = [];
 
 function rollDice(selectedDice) {
-    console.log(selectedDice)
-
     const diceNumber = Math.floor(Math.random() * selectedDice) + 1;
 
     currentRollInput.value = diceNumber;
 
-
     previousRolls.push(diceNumber);
 
-
     if (previousRolls.length > 10) {
-    previousRolls.shift();
-}
-
+        previousRolls.shift();
+    }
 
     previousRollsDiv.innerHTML = "Previous Rolls: " + previousRolls;
 }
 
-
-
-
 diceInputs.forEach(input => {
-input.addEventListener('change', () => {
-    if (input.checked) {
-        selectedDice = input.id;
-        console.log("hello")
-        console.log(input.id)
-        console.log(selectedDice)
-    /*rollDice(input.id);*/
+    input.addEventListener('change', () => {
+        if (input.checked) {
+            selectedDice = parseInt(input.value);
+            diceImage.src = `./assets/img/d${selectedDice}.png`;
+            diceImage.classList.add('visible'); // Show the dice image
+            currentRollInput.value = ''; 
+        }
+    });
+});
+
+rollbtn.addEventListener('click', () => {
+    if (diceImage.classList.contains('visible')) {
+        rollDice(selectedDice);
     }
 });
-});
-
-rollbtn.addEventListener('click', () => {rollDice(selectedDice)})
