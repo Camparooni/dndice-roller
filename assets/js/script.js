46e9c2e7ca9fdf613233ca6528ca7cef8af7e409
IsaiahW <i.m.waddingtonsafem@gmail.com>
2023-07-27 21:42:56 -0500

    added api for RNG

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,58 @@
-const rollbtn = document.getElementById('rollbtn');
-const currentRollInput = document.getElementById('currentRoll');
-const previousRollsDiv = document.getElementById('previousRolls');
-const diceInputs = document.querySelectorAll('input[type="checkbox"]');
-let selectedDice = 6; // Default to D6
+async function getRandomNumber(min, max) {
+  try {
+    const response = await fetch(`https://www.randomnumberapi.com/api/v1.0/random?min=${min}&max=${max}&count=1`);
 
-let previousRolls = [];
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
 
-function rollDice(selectedDice) {
-    console.log(selectedDice)
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error('Error fetching random number:', error);
+    return null;
+  }
+}
 
-  const diceNumber = Math.floor(Math.random() * selectedDice) + 1;
+async function rollDice(selectedDice) {
+  const min = 1;
+  const max = selectedDice;
+
+  const diceNumber = await getRandomNumber(min, max);
 
   currentRollInput.value = diceNumber;
 
-
   previousRolls.push(diceNumber);
-
 
   if (previousRolls.length > 10) {
     previousRolls.shift();
   }
 
-
-  previousRollsDiv.innerHTML = "Previous Rolls: " + previousRolls;
+  previousRollsDiv.innerHTML = previousRolls;
 }
 
+const rollbtn = document.getElementById('rollbtn');
+const currentRollInput = document.getElementById('currentRoll');
+const previousRollsDiv = document.getElementById('previousRolls');
+const diceInputs = document.querySelectorAll('input[type="radio"]'); // Corrected the selector for radio inputs
+const diceImage = document.getElementById('diceimage');
+let selectedDice = 6; // Default to D6
 
-
+let previousRolls = [];
 
 diceInputs.forEach(input => {
   input.addEventListener('change', () => {
     if (input.checked) {
-      selectedDice = input.id;
-      console.log("hello")
-      console.log(input.id)
-      console.log(selectedDice)
-      
-      /*rollDice(input.id);*/
+      selectedDice = parseInt(input.value);
+      diceImage.src = `./assets/img/d${selectedDice}.png`;
+      diceImage.classList.add('visible'); // Show the dice image
+      currentRollInput.value = '';
     }
   });
 });
 
-rollbtn.addEventListener('click', () => {rollDice(selectedDice)})
+rollbtn.addEventListener('click', () => {
+  if (diceImage.classList.contains('visible')) {
+    rollDice(selectedDice);
+  }
+});
