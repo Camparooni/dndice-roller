18b9870da10beecb1051f82894c063cbfa623769
IsaiahW <i.m.waddingtonsafem@gmail.com>
2023-07-27 22:08:45 -0500

    fixed code that matteo broke

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+async function getRandomNumber(min, max) {
+  try {
+    const response = await fetch(`https://www.randomnumberapi.com/api/v1.0/random?min=${min}&max=${max}&count=1`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error('Error fetching random number:', error);
+    return null;
+  }
+}
+
+async function rollDice(selectedDice) {
+  const min = 1;
+  const max = selectedDice;
+
+  const diceNumber = await getRandomNumber(min, max);
+
+  currentRollInput.value = diceNumber;
+
+  previousRolls.push(diceNumber);
+
+  if (previousRolls.length > 10) {
+    previousRolls.shift();
+  }
+
+  previousRollsDiv.innerHTML = previousRolls;
+}
+
 const rollbtn = document.getElementById('rollbtn');
 const currentRollInput = document.getElementById('currentRoll');
 const previousRollsDiv = document.getElementById('previousRolls');
@@ -5,37 +38,21 @@ const diceInputs = document.querySelectorAll('input[type="radio"]'); // Correcte
 const diceImage = document.getElementById('diceimage');
 let selectedDice = 6; // Default to D6
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-function rollDice(selectedDice) {
-    const diceNumber = Math.floor(Math.random() * selectedDice) + 1;
-
-    currentRollInput.value = diceNumber;
-
-    previousRolls.push(diceNumber);
-
-    if (previousRolls.length > 10) {
-        previousRolls.shift();
-    }
-
-    previousRollsDiv.innerHTML = previousRolls;
-}
+let previousRolls = [];
 
 diceInputs.forEach(input => {
-    input.addEventListener('change', () => {
-        if (input.checked) {
-            selectedDice = parseInt(input.value);
-            diceImage.src = `./assets/img/d${selectedDice}.png`;
-            diceImage.classList.add('visible'); // Show the dice image
-            currentRollInput.value = ''; 
-        }
-    });
+  input.addEventListener('change', () => {
+    if (input.checked) {
+      selectedDice = parseInt(input.value);
+      diceImage.src = `./assets/img/d${selectedDice}.png`;
+      diceImage.classList.add('visible'); // Show the dice image
+      currentRollInput.value = '';
+    }
+  });
 });
 
 rollbtn.addEventListener('click', () => {
-    if (diceImage.classList.contains('visible')) {
-        rollDice(selectedDice);
-    }
+  if (diceImage.classList.contains('visible')) {
+    rollDice(selectedDice);
+  }
 });
